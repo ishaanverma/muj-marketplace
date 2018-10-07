1ef8067f3f6b4de067edd8973df9c9d3a3d0cad2
Ishaan Verma <ishaanverma97@gmail.com>
2018-10-08 01:12:43 +0530

    Moved all the templates to one place

--- src/marketplace/products/forms.py
+++ src/marketplace/products/forms.py
@@ -1,1 +1,25 @@
 from django import forms
+from .models import Product
+
+class AddProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['title', 'description', 'price', 'image']
+        widgets = {
+    		"title": forms.TextInput(attrs={
+                            "class": "form-control",
+                            "placeholer": "Enter Name",
+                        }),
+    		"description": forms.Textarea(attrs={
+                            "class": "form-control",
+                            "placeholer": "Enter Description",
+                        }),
+    		"price": forms.NumberInput(attrs={
+                            "class": "form-control",
+                            "placeholer": "Enter Price",
+                        }),
+    		"image": forms.FileInput(attrs={
+                            "class": "form-control-file",
+    						"type": "file",
+                        }),
+    	}
